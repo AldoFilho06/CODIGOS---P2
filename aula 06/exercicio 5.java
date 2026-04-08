c4bbfcc25eb8bc229e311d87a70bb7747057c2ae
Aldo Filho <aldofilhom@gmail.com>
2026-04-08 11:19:58 -0300

    Create exercicio 5.java

---
+++ aula 06/exercicio 5.java
@@ -0,0 +1,9 @@
+public class exercicio5 {
+    public static void main(String[] args) {
+        String palavra = "Computação";
+
+        System.out.println("Primeiro: " + palavra.charAt(0));
+        System.out.println("Terceiro: " + palavra.charAt(2));
+        System.out.println("Ultimo: " + palavra.charAt(9));
+    }
+}
