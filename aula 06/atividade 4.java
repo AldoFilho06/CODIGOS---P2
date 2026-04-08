c1b3b08110ce11c9ead6c9e6476247b76ecf3201
Aldo Filho <aldofilhom@gmail.com>
2026-04-08 11:15:36 -0300

    Create atividade 4.java

---
+++ aula 06/atividade 4.java
@@ -0,0 +1,9 @@
+public class exercicio4 {
+    public static void main(String[] args) {
+
+        String palavra = "Programação";
+
+        System.out.println(palavra.substring(0,8));
+        System.out.println(palavra.substring(8));
+    }
+}
